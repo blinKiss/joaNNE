bc4180807f4ed8fd24ee149090be56dbbc95b29f
blinKiss <blinkiss@naver.com>
2023-05-25 09:07:43 +0900

    25

---
+++ tf1_tensor1.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+scalar = tf.constant(1)
+vector = tf.constant([1,2,3])
+matrix = tf.constant([[1,2,3], [4,5,6], [7,8,9]])
+tensor = tf.constant([[[1,2,3], [4,5,6], [7,8,9]],
+                      [[1,2,3], [4,5,6], [7,8,9]],
+                      [[1,2,3], [4,5,6], [7,8,9]]])
+
+print(tf.rank(scalar))
+print(tf.rank(vector))
+print(tf.rank(matrix))
+print(tf.rank(tensor))
---
+++ tf1_tensor2.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+a = tf.zeros(1)
+b = tf.zeros([2])
+c = tf.zeros([2, 3])
+
+print(a,b,c)
