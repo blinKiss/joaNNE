22fd554068b48bebe145acd8040c7364be38493b
blinKiss <blinkiss@naver.com>
2023-05-30 13:37:59 +0900

    30

--- tf2_tensor.py
+++ tf2_tensor.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-simple_model = keras.Sequential([keras.layers.Dense(units = 1, input_shape = [1])])
+simple_model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 
 # training data
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0], dtype=float)
